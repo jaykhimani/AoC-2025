69eb38903f80ac4f9f0ce60c9f54da8c483d9088
Jay Khimani <jay.khimani@ing.com>
2025-12-03 07:00:24 +0100

    day02: solution p2

--- src/main/kotlin/day02/Day02.kt
+++ src/main/kotlin/day02/Day02.kt
@@ -1,9 +1,10 @@
 package day02
 
+import utils.log
 import utils.readInput
 
 fun main() {
-    val inputLine = readInput("day02", false)[0]
+    val inputLine = readInput("day02")[0]
     val debug = false
     println("Part 1: answer: ${p1(inputLine, debug)}")
 
@@ -11,7 +12,24 @@ fun main() {
 }
 
 private fun p2(inputLine: String, debug: Boolean): Long {
-    TODO()
+    val regex = Regex("^(\\d+)\\1+$")
+
+    val ranges = inputLine.split(",")
+    var total = 0L
+    ranges.forEach { range ->
+        val lbUb = range.split("-")
+        val lb = lbUb[0].trim().toLong()
+        val ub = lbUb[1].trim().toLong()
+
+        for (i in lb..ub) {
+            val id = i.toString()
+            if (regex.containsMatchIn(id)) {
+                log("Found match for $id", debug)
+                total += i
+            }
+        }
+    }
+    return total
 }
 
 private fun p1(inputLine: String, debug: Boolean): Long {
